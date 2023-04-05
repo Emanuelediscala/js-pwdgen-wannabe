b471d427878717cb60c47218e11f07afd8d00278
Emanuele discala <manueldiscala@gmail.com>
2023-04-05 16:12:02 +0200

    added switch function and button prompt

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 
+function pwngen() {
 // NOME INPUT
 
 let nome = prompt ("Inserisci il tuo nome");
@@ -22,3 +23,16 @@ let password = `${nome}${cognome}${colorepreferito}${annocorrente}`;
 // Function calc
 
 document.getElementById("password").innerText = password;
+
+switch (colorepreferito) {
+    case "rosso":
+        document.getElementById("password").style.backgroundColor = "red";
+        break
+    case "giallo":
+        document.getElementById("password").style.backgroundColor = "yellow";
+        break
+    case "verde":
+        document.getElementById("password").style.backgroundColor = "green";
+        break
+}
+}
